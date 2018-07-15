f41202dcb1fe814a4aa7f73d7605e3d88665f90b
Remi PINOIT <rpinoit@e2r12p11.42.fr>
2018-07-15 16:31:51 +0200

    malloc map and it's almost finished

--- srcs/wolf3d_parser.c
+++ srcs/wolf3d_parser.c
@@ -6,35 +6,50 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/15 15:55:52 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/07/15 16:12:57 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/07/15 16:31:07 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf.h"
 
-t_bool  create_map()
+t_bool  create_map(int **map, t_list *lst)
 {
+    t_list *tmp;
+
+    tmp = lst;
+    (void)map;
+    ft_lstdel(&tmp, ft_deltab);
     return (TRUE);
 }
 
-t_bool  read_map(int **map, char **line, int fd)
+t_bool  read_map(char **line, t_list **lst, int fd)
 {
-    (void)map;
+    char    **split;
+
     while (get_next_line(fd, line) > 0)
+    {
+        split = ft_strsplit(*line, ' ');
         ft_strdel(&(*line));
+        if (*lst != NULL)
+            ft_lstadd(lst, ft_lstnew((void *)split, 0));
+        else
+            *lst = ft_lstnew((void *)split, 0);
+    }
     return (TRUE);
 }
 
 t_bool  wolf3d_parser(int **map, char *path)
 {
+    t_list  *lst;
     char    *line;
     int     fd;
 
-    if ((fd = open(path, O_RDONLY)) <= -1)
+    lst = NULL;
+    if ((fd = open(path, O_RDONLY)) <= 0)
         return (TRUE);
-    if (read_map(map, &line, fd) == FALSE)
+    if (read_map(&line, &lst, fd) == FALSE)
         return(FALSE);
-    if (create_map() == FALSE)
+    if (create_map(map, lst) == FALSE)
         return (FALSE);
     if (close(fd) == -1)
         return (FALSE);
